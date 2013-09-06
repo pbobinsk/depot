60189a94b1cbd231fead04b57ad0e39e881c45e6
Piotr Bobinski <pbobinsk@gmail.com>
2013-09-06 14:30:18 +0200

    Conditional counter display

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,8 @@
 class StoreController < ApplicationController
+include StoreHelper
   def index
-    #@count = increment_count
+    @count = increment_count
+    @count_message = "You've accessed this page #{view_context.pluralize(@count , 'time')}" if session[:counter] > 5
     @products = Product.order(:title)
   end
 end
