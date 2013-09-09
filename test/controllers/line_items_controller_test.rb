13fbd5dbcb6680e7eba78fa76a9a9bb27762006e
Piotr Bobinski <pbobinsk@gmail.com>
2013-09-09 14:35:44 +0200

    Testing Ajax and removing errors.

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,11 +17,21 @@ class LineItemsControllerTest < ActionController::TestCase
   end
 
   test "should create line_item" do
-    assert_difference('LineItem.count') do
+    assert_difference('LineItem.count', 1) do
       post :create, product_id: products(:ruby).id
     end
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path
   end
+
+  test "should create line_item twice" do
+    assert_difference('LineItem.count', 1) do
+      post :create, product_id: products(:ruby).id
+      post :create, product_id: products(:ruby).id
+    end
+    assert_redirected_to store_path
+  end
+
+
 
   test "should show line_item" do
     get :show, id: @line_item
